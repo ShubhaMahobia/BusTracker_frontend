6063a63b95ffed8e33912a42bf3d5fe9b3c9a3b5
Shubham Mahobia <mahobiashubham4@gmail.com>
2023-11-01 23:20:48 +0530

    Login screen ui design 1.0

---
+++ bus_tracker/lib/core/Authentication/screens/login/login_screen.dart
@@ -0,0 +1,137 @@
+import 'package:bus_tracker/common_widgets/Buttons/button_two.dart';
+import 'package:bus_tracker/constants/custom_fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            color: Colors.lightBlue,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+          ),
+          Positioned(
+            top: 60,
+            left: 15,
+            child: GestureDetector(
+              onTap: () {},
+              child: const Icon(
+                Icons.arrow_back_sharp,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 50,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              width: MediaQuery.of(context).size.width * 0.75,
+              height: MediaQuery.of(context).size.height * 0.625,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Text(
+                    'Login To Your Account',
+                    style: CustomTextStyle.t5(context),
+                  ),
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  Container(
+                    width: 250,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.lightBlue),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Center(
+                      child: TextField(
+                        style: CustomTextStyle.t6(context),
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.all(8),
+                          hintText: 'Email',
+                          hintStyle: CustomTextStyle.hintText(context),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    width: 250,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.lightBlue),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Center(
+                      child: TextField(
+                        obscureText: true,
+                        style: CustomTextStyle.t6(context),
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.all(8),
+                          hintText: 'Password',
+                          hintStyle: CustomTextStyle.hintText(context),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: const SizedBox(
+                      height: 5,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25),
+                        child: Text(
+                          'Forget Password?',
+                          style: CustomTextStyle.hintText(context),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Button2(buttonText: 'L O G I N', onPressed: () {}),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Container(
+                      height: 3,
+                      color: Colors.lightBlue,
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 20, horizontal: 15),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- bus_tracker/lib/main.dart
+++ bus_tracker/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:bus_tracker/core/Authentication/screens/onboarding_screen.dart';
+import 'package:bus_tracker/core/Authentication/screens/login/login_screen.dart';
+import 'package:bus_tracker/core/Authentication/screens/onboarding/onboarding_screen.dart';
 import 'package:bus_tracker/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -9,8 +10,31 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    initialization();
+    super.initState();
+  }
+
+  void initialization() async {
+    // ignore_for_file: avoid_print
+    print('ready in 3...');
+    await Future.delayed(const Duration(milliseconds: 300));
+    print('ready in 2...');
+    await Future.delayed(const Duration(milliseconds: 200));
+    print('ready in 1...');
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('go!');
+    FlutterNativeSplash.remove();
+  }
 
   // This widget is the root of your application.
   @override
@@ -20,7 +44,7 @@ class MyApp extends StatelessWidget {
       theme: CustomTheme.lightTheme,
       darkTheme: CustomTheme.darkTheme,
       themeMode: ThemeMode.system,
-      home: const OnboardingScreen(),
+      home: const LoginScreen(),
     );
   }
 }
